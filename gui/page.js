d3717e567b62c5c93b2e1f544310beab91990176
xiao_e_yun <67814228+QAZXSW517@users.noreply.github.com>
2021-04-24 14:06:03 +0800

    a

--- gui/page.js
+++ gui/page.js
@@ -1,25 +1,24 @@
-window.onresize = function (){
+window.onresize = function () {
     window.resizeTo(300, 600);
 }
 
 footer=$('footer')
 main=$('main#main_contant')
 
-$('#menu_toggle').on('click',function(){
+$('#menu_toggle').on('mousedown',function(){
     footer.slideToggle()
 })
 
 tmp={};
 footer.on('click','button',function(){
     footer.slideUp()
-    var id=this.id
-    console.log("open \""+id+"\" page")
-    $.get("/page/"+id+".html",function(data){
-        main.off() //刪除監聽
-        main.html(data) //寫入數據
-        if(tmp[id]){
-            $.getScript("/page/"+id+".js")
-        }
-        eval(id())
-    })
+    load_page(this.id)
 })
+
+function load_page(id, href = false) {
+    console.log("open \"" + id + "\" page")
+    $.get("/page/" + id + ".html" , function (data) {
+        main.off() //刪除監聽
+        main.html(data) 
+    })
+}
--- index.py
+++ index.py
@@ -1,11 +1,4 @@
 import eel
-import re
-import json
-import base64
-import numpy as np
-from PIL import Image
-from io import BytesIO
-
 
 eel.init('gui') 
 eel.start('index.html',suppress_error=True,size = (300,600))
